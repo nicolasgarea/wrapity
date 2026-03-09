84bfdbc008c54fd60f621aae4ac2a75d4c15ea10
Nicolás Garea <nicogareacidre@gmail.com>
2026-03-09 19:44:47 +0100

    feat: add user profile (#10)
    closes #9

---
+++ backend/app/core/dependencies.py
@@ -0,0 +1,30 @@
+from fastapi import Depends
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from sqlalchemy.orm import Session
+
+from app.core.exceptions import InvalidTokenException
+from app.core.security import decode_access_token
+from app.db.database import get_db
+from app.models.user import User
+from app.repositories.user_repositories import UserRepository
+
+
+bearer_scheme = HTTPBearer()
+
+
+def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(bearer_scheme),
+    db: Session = Depends(get_db),
+) -> User:
+    token_data = decode_access_token(credentials.credentials)
+    user_id = token_data.get("sub")
+
+    if not user_id:
+        raise InvalidTokenException("Invalid token")
+
+    user_repository = UserRepository(db)
+    user = user_repository.get_user_by_id(int(user_id))
+
+    if user is None:
+        raise InvalidTokenException("Invalid token")
+    return user
---
+++ backend/app/routes/user_routes.py
@@ -0,0 +1,31 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from app.core.dependencies import get_current_user
+from app.db.database import get_db
+from app.models.user import User
+from app.repositories.user_repositories import UserRepository
+from app.schemas.user_schemas import UserResponse, UserUpdate
+from app.services.user_services import UserService
+
+
+router = APIRouter(prefix="/users", tags=["me"])
+
+
+def get_user_service(db: Session = Depends(get_db)) -> UserService:
+    repo = UserRepository(db)
+    return UserService(repo)
+
+
+@router.get("/me", response_model=UserResponse)
+def get_me(current_user: User = Depends(get_current_user)) -> UserResponse:
+    return current_user
+
+
+@router.patch("/me", response_model=UserResponse)
+def update_me(
+    user_update: UserUpdate,
+    current_user: User = Depends(get_current_user),
+    user_service: UserService = Depends(get_user_service),
+) -> UserResponse:
+    updated_user = user_service.update_user(current_user, user_update)
+    return updated_user
